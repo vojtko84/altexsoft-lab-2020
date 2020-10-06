562b56e43374d4aaa0f1ae59b6fc8d0230fb56b2
vojtko84 <62484094+vojtko84@users.noreply.github.com>
2020-10-07 01:07:55 +0300

    fixed CTE

--- task3/queries/SQLQuery.sql
+++ task3/queries/SQLQuery.sql
@@ -1,8 +1,16 @@
-SELECT dbo.Recipe.Name, dbo.Recipe.Description, dbo.Ingredient.Name 
-FROM dbo.Recipe
-JOIN dbo.RecipeIngredient
-ON dbo.RecipeIngredient.RecipeId = dbo.Recipe.Id
-JOIN dbo.Ingredient
-ON  dbo.Ingredient.Id = dbo.RecipeIngredient.IngredientId
-WHERE dbo.Recipe.Id IN (SELECT TOP 3 dbo.Recipe.Id FROM dbo.Recipe
-WHERE dbo.Recipe.CategoryId = 3)
+WITH  Category_CTE AS
+(
+	SELECT * FROM dbo.Category
+	WHERE dbo.Category.Id = 3
+	UNION ALL
+	SELECT dbo.Category.Id, dbo.Category.Name, dbo.Category.ParentId FROM dbo.Category
+	JOIN Category_CTE ON dbo.Category.ParentId = Category_CTE.Id
+)
+SELECT x.Name, dbo.Ingredient.Name FROM
+(
+	SELECT TOP 3 dbo.Recipe.* FROM dbo.Recipe
+	JOIN Category_CTE ON dbo.Recipe.CategoryId = Category_CTE.Id
+	ORDER BY Recipe.CategoryId
+) AS x
+JOIN dbo.RecipeIngredient ON x.Id = dbo.RecipeIngredient.RecipeId
+JOIN dbo.Ingredient ON dbo.RecipeIngredient.IngredientId = dbo.Ingredient.Id
--- task3/schema/1Category.sql
+++ task3/schema/1Category.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].[Category](
 	[Id] [int] IDENTITY(1,1) NOT NULL,
 	[Name] [nvarchar](50) NOT NULL,
+	[ParentId] [int] NULL,
  CONSTRAINT [PK_Category] PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
@@ -10,5 +11,11 @@
 	[Name] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Category]  WITH CHECK ADD  CONSTRAINT [FK_Category_Category] FOREIGN KEY([ParentId])
+REFERENCES [dbo].[Category] ([Id])
+GO
+
+ALTER TABLE [dbo].[Category] CHECK CONSTRAINT [FK_Category_Category]
 GO
 
